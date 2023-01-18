2ba0545cef73b59bf5fb1092770a9b1d947dcbdd
Abdur Rakib Shaikh <86706284+whispersrakib@users.noreply.github.com>
2023-01-18 11:31:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ let bar = document.querySelector('#bar');
 let sidebar =document.querySelector('.sidebar');
 let close =document.querySelector('.close');
 
-
-
-
 if(bar){
     bar.addEventListener('click',()=>{
         sidebar.classList.add('active');
@@ -16,5 +13,33 @@ if(bar){
 if(close){
     close.addEventListener('click',()=>{
         sidebar.classList.remove('active');
+    })
+}
+
+let wj =document.querySelectorAll('.wj');
+let sh=document.querySelectorAll('.sh');
+let mc=document.querySelectorAll('.mc');
+const kc =document.querySelectorAll('.kc');
+for (const k of kc) {
+    k.addEventListener('click',()=>{
+        window.location="singleKclothes.html";
+    })
+    
+}
+for (const m of mc) {
+    m.addEventListener('click',()=>{
+        window.location="singleMclothes.html";
+    })
+    
+}
+for(let s of sh){
+    s.addEventListener('click',()=>{
+        window.location="singleShoes.html";
+    })
+}
+
+for(let w of wj){
+    w.addEventListener('click',()=>{
+        window.location="wjacketsing.html";
     })
 }
